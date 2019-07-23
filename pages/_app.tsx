c565d9a0484d80e13ce5ca1ee76fac6fe51e3152
mlijanto@microsoft.com <mlijanto@microsoft.com>
2019-07-23 16:47:12 +0700

    Filling the head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import App, { Container } from "next/app";
+import Head from "next/head";
 import "../static/css/normalize.css";
 import "../static/css/global.css";
 
@@ -8,9 +9,27 @@ export default class Site extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <Container>
-        <Component {...pageProps} />
-      </Container>
+      <>
+        <Head>
+          <title>Martin Lijanto</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png" />
+          <link rel="manifest" href="/public/site.webmanifest" />
+          <link rel="mask-icon" href="/public/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="icon" href="/public/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#181818" />
+          <meta name="msapplication-config" content="/public/browserconfig.xml" />
+          <meta name="theme-color" content="#181818" />
+          <meta property="og:image" content="/public/ml-og.png" />
+          <meta property="og:description" content="The personal site of Martin Lijanto." />
+          <meta property="og:title" content="Martin Lijanto" />
+          <meta name="twitter:title" content="Martin Lijanto" />
+        </Head>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </>
     );
   }
 }
